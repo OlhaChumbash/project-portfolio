991cfe93e4a8f755c214b8fff7fd6a0a3f719126
Olha_Chumbash <olyachumbash@gmail.com>
2024-08-13 18:44:57 +0200

    fix contacts

--- react-portfolio/src/components/Hero/Hero.jsx
+++ react-portfolio/src/components/Hero/Hero.jsx
@@ -4,9 +4,9 @@ import styles from "./Hero.module.css";
 import { getImageUrl } from "../../utils";
 
 export const Hero = () => {
-  const { i18n } = useTranslation();
+  const { t, i18n } = useTranslation();
 
- 
+
   const resumeUrl = i18n.language === 'en' 
     ? "assets/hero/Olha_Chumbash_resume_en.docx" 
     : "assets/hero/Olha_Chumbash_resume_ua.docx";
@@ -14,22 +14,21 @@ export const Hero = () => {
   return (
     <section className={styles.container}>
       <div className={styles.content}>
-        <h1 className={styles.title}>Hi, I'm Olha</h1>
+        <h1 className={styles.title}>{t('hero_title')}</h1>
         <p className={styles.description}>
-          I'm a trainee full-stack developer with a strong enthusiasm for learning and growth. 
-          I’m passionate about exploring new technologies and applying my skills to real-world projects.
+          {t('hero_description')}
         </p>
         <a 
           href={resumeUrl}  // Используем динамически изменяемый путь
           download 
           className={styles.contactBtn}
         >
-          Download my Resume
+          {t('hero_resume_button')}
         </a>
       </div>
       <img
         src={getImageUrl("hero/heroImage.png")}
-        alt="Hero image of me"
+        alt={t('about_imageAlt')}
         className={styles.heroImg}
       />
       <div className={styles.topBlur} />
